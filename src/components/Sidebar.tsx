accbd52826852d0ddc05765142e1d28a146527c3
NuanceAiDev <info@nuancedigitalsolutions.com>
2025-11-03 14:56:25 +0530

    Updated Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -38,7 +38,7 @@ export function Sidebar({
             <img 
               src="/pp copy copy copy.jpg" 
               alt="Nuance Digital" 
-              className="h-20 w-auto"
+              className="h-25 w-auto"
             />
           </div>
           <button
