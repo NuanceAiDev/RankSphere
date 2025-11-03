f86555edae81e1cfe2ea5c6012c72e5a8d9ff726
NuanceAiDev <info@nuancedigitalsolutions.com>
2025-11-03 12:45:39 +0530

    Updated Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Plus, Moon, Sun, Users, TrendingUp, Edit2, Trash2, Search } from 'lucide-react';
+import { Plus, Moon, Sun, Users, TrendingUp, CreditCard as Edit2, Trash2, Search } from 'lucide-react';
 import { useTheme } from '../contexts/ThemeContext';
 import { Client } from '../types';
 
