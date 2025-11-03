9988170c5a73dd3a6e32632adbb608664bf1fea0
NuanceAiDev <info@nuancedigitalsolutions.com>
2025-11-03 14:46:37 +0530

    Increase sidebar logo size

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -38,7 +38,7 @@ export function Sidebar({
             <img 
               src="/pp.jpg" 
               alt="Nuance Digital" 
-              className="h-10 w-auto"
+              className="h-16 w-auto"
             />
           </div>
           <button
